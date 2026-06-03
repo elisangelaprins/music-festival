b43bdde35a5e2d50303b827b805e0591af96ea4f
Andryelli-Lourenco <@andryelleafgl@gmail.com>
2026-06-03 14:02:50 -0300

    feat: cria controller patrocinador

--- src/controller/PatrocinadorController.java
+++ src/controller/PatrocinadorController.java
@@ -1,4 +1,58 @@
 package controller;
+import java.util.ArrayList;
+import model.Patrocinador;
 
 public class PatrocinadorController {
+
+
+    private ArrayList<Patrocinador> patrocinadores;
+
+
+    public PatrocinadorController() {
+        patrocinadores = new ArrayList<>();
+    }
+
+    public void adicionarPatrocinador(Patrocinador patrocinador) {
+        patrocinadores.add(patrocinador);
+    }
+
+    public ArrayList<Patrocinador> listarPatrocinadores() {
+        return patrocinadores;
+    }
+
+    public Patrocinador buscarPatrocinador(int id) {
+        for (Patrocinador patrocinador: patrocinadores) {
+            if (patrocinador.getId() == id) {
+                return patrocinador;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean atualizarCategoria(int id, String categoria) {
+
+        Patrocinador patrocinador = buscarPatrocinador(id);
+
+        if (patrocinador != null) {
+            patrocinador.setCategoria(categoria);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean removerPatrocinador(int id) {
+
+        Patrocinador patrocinador = buscarPatrocinador(id);
+
+        if (patrocinador != null) {
+            patrocinadores.remove(patrocinador);
+            return true;
+        }
+
+        return false;
+    }
+
+
 }
